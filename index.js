e8de481b38faebb4732f7a90a02e1c8bfeccc177
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2016-06-11 01:14:43 +0200

    Now working correctly for pub/sub

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-var _ = require('lodash');
 var amqp = require('amqp');
 
 module.exports.attach = function (broker) {
   var brokerOptions = broker.options.brokerOptions;
   var instanceId = broker.instanceId;
 
-  var channelQueueMap = {};
+  var resetErrorHandler = function (client) {
+    client.removeAllListeners('error');
+    client.on('error', function (err) {
+      broker.emit('error', err);
+    });
+  };
+
+  var destroyChannelExchange = function (channelName) {
+    var rabbitExchange = rabbitClient.exchange(channelName, {noDeclare: true, passive: true});
+    resetErrorHandler(rabbitClient);
+    var rabbitQueue = rabbitClient.queue('', {exclusive: true});
+    rabbitExchange.destroy();
+  };
+
+  var queueMap = {};
 
   var rabbitClient = amqp.createConnection(brokerOptions);
+
   rabbitClient.on('ready', function () {
     broker.on('subscribe', function (channelName) {
-      if (!channelQueueMap[channelName]) {
-        rabbitClient.queue(channelName, function (rabbitQueue) {
-          rabbitQueue.subscribe(function (packet) {
-            if (packet && packet.instanceId != instanceId) {
-              broker.publish(channelName, packet.data);
-            }
-          }).addCallback(function (ok) {
-            channelQueueMap[channelName] = {
-              consumerTag: ok.consumerTag
-            };
+      rabbitClient.exchange(channelName, {type: 'fanout', durable: true, autoDelete: false}, function (rabbitExchange) {
+        if (!queueMap[channelName]) {
+          queueMap[channelName] = rabbitClient.queue('', {exclusive: true}, function (rabbitQueue) {
+            rabbitQueue.bind(rabbitExchange, '');
+            rabbitQueue.subscribe(function (packet) {
+              if (packet && packet.instanceId != instanceId) {
+                broker.publish(channelName, packet.data);
+              }
+            })
           });
-        });
-      }
+        }
+      });
     });
     broker.on('unsubscribe', function (channelName) {
-      var rabbitQueue = rabbitClient.queue(channelName);
-      if (rabbitQueue && channelQueueMap[channelName]) {
-        rabbitQueue.unsubscribe(channelQueueMap[channelName].consumerTag);
-        delete channelQueueMap[channelName];
-      }
+      destroyChannelExchange(channelName);
     });
     broker.on('publish', function (channelName, data) {
       var packet = {
         instanceId: instanceId,
         data: data
       };
-      rabbitClient.publish(channelName, packet, {
-        contentType: 'application/json'
+      rabbitClient.exchange(channelName, {type: 'fanout', durable: true, autoDelete: false}, function (rabbitExchange) {
+        rabbitExchange.publish('', packet, {
+          contentType: 'application/json'
+        });
       });
     });
   });
